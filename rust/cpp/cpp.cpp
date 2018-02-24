7779d051584134475fbbb670e6504b946dface1b
Anders Schau Knatten <anders@knatten.org>
2018-02-24 10:41:55 +0100

    tmp

--- rust/cpp/cpp.cpp
+++ rust/cpp/cpp.cpp
@@ -12,60 +12,23 @@ struct Result {
 //snippetysnip_end
 
 int* get();
-void call_get();
 bool get_in_argument(int*&);
-void call_get_in_argument();
-void call_get_or_throw();
 int* get_or_throw();
 Result<int> get_result();
+
+void call_get();
+void call_get_in_argument();
+void call_get_or_throw();
+void call_get_result();
+
+void print(int* i);
 
 int main() {
     cout << "------" << boolalpha << endl;
     /* call_get(); */
     /* call_get_in_argument(); */
-    call_get_or_throw();
-
-
-    /* Result<int> i7 = get_result(); //true, 0x10d0e509c */
-    /* Result<int> i8 = get_result(); //false, null */
-}
-
-void call_get() {
-    //snippetysnip_begin:call_get
-    int* success = get();
-    cout << success << endl; //0x101d78118
-    cout << *success << endl; //1
-
-    int* failure = get();
-    cout << failure << endl; //0x0
-    cout << *failure << endl; //segfault!
-    //snippetysnip_end
-}
-
-void call_get_in_argument() {
-    //snippetysnip_begin:call_get_in_argument
-    int* success;
-    bool ok = get_in_argument(success);
-    cout << ok << " " << success << endl; //true 0x10d3ce11c
-    cout << *success << endl; //1
-
-    int* failure = nullptr;
-    bool ok2 = get_in_argument(failure);
-    cout << ok2 << " " << failure << endl;  //false 0x0
-    cout << *failure << endl; //segfault!
-    //snippetysnip_end
-}
-
-void call_get_or_throw() {
-    //snippetysnip_begin:call_get_or_throw
-    int* success = get_or_throw();
-    cout << success << endl; //0x101d78118
-    cout << *success << endl; //1
-
-    int* failure = get_or_throw(); //exception
-    cout << failure << endl; //never reached
-    cout << *failure << endl; //never reached
-    //snippetysnip_end
+    /* call_get_or_throw(); */
+    call_get_result();
 }
 
 //snippetysnip_begin:get
@@ -98,9 +61,72 @@ int* get_or_throw() {
 }
 //snippetysnip_end
 
+//snippetysnip_begin:get_result
 Result<int> get_result() {
     static int i = 0;
     return i++ == 0
         ? Result<int>{true, &i}
         : Result<int>{false, nullptr};
 }
+//snippetysnip_end
+
+void call_get() {
+    //snippetysnip_begin:call_get
+    int* succeeding = get();
+    if (succeeding != nullptr)
+        print(succeeding); //0x1035f5118, 1 
+
+    int* failing = get();
+    if (failing != nullptr)
+        print(failing); //Would have been: 0x0, segfault
+
+    //snippetysnip_end
+}
+
+void call_get_in_argument() {
+    //snippetysnip_begin:call_get_in_argument
+    int* succeeding;
+    bool ok = get_in_argument(succeeding);
+    if (ok)
+        print(succeeding); //0x10fe1a11c, 1
+
+    int* failing = nullptr;
+    bool ok2 = get_in_argument(failing);
+    if (!ok2)
+        print(failing); //Would have been: 0x0, segfault
+
+    //snippetysnip_end
+}
+
+void call_get_or_throw() {
+    //snippetysnip_begin:call_get_or_throw
+    try {
+        int* succeeding = get_or_throw();
+        print(succeeding); //0x10756d120, 1
+    } catch (...) {}
+
+    try {
+        int* failing = get_or_throw(); //exception
+        print(failing); //Would have been: 0x0, segfault
+    } catch (...) {}
+    //snippetysnip_end
+}
+
+void call_get_result() {
+    //snippetysnip_begin:call_get_result
+    Result<int> succeeding = get_result();
+    if (succeeding.ok)
+        print(succeeding.pointer); //0x1063c1124, 1
+
+    Result<int> failing = get_result();
+    if (!failing.ok)
+        print(failing.pointer); //Would have been: 0x0, segfault
+    //snippetysnip_end
+}
+
+//snippetysnip_begin:print
+void print(int* i) {
+    cout << i << endl;
+    cout << *i << endl;
+}
+//snippetysnip_end
